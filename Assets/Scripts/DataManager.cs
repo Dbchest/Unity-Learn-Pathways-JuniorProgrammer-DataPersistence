f7e01c26e4fceec27c44e41ebee484342590e39c
Derek <56432831+Dbchest@users.noreply.github.com>
2021-06-05 08:34:59 -0400

    Initial commit
    Added DataManager.cs

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
