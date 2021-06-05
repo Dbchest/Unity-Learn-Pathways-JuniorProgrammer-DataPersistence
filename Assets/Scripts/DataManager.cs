714d63cc41bed2a8de8979eac640e30308ecf41f
Derek <56432831+Dbchest@users.noreply.github.com>
2021-06-05 08:47:15 -0400

    Update DataManager.cs
    Added Save and Load methods to DataManager.cs
    
    Renamed UserData.cs to Data.cs

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -4,6 +4,16 @@ public class DataManager : MonoBehaviour
 {
     public static DataManager Instance { get; private set; }
 
+    public void Save()
+    {
+
+    }
+
+    public void Load()
+    {
+
+    }
+
     private void Awake()
     {
         if (Instance == null)
